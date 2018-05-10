e5687023d83eb17488d826ef79885d288115d749
wizubizu <odugbesanabimbola@gmail.com>
2018-05-10 16:25:03 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,6 +134,18 @@ $(window).scroll(function() {
        });
     });
 
+     $(document).ready(function () {
+      //called when key is pressed in textbox
+      $("#quantity1").keypress(function (e) {
+         //if the letter is not digit then display error and don't type anything
+         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            //display error message
+            $("#errmsg1").html("Numbers Only").show().fadeOut("slow");
+                   return false;
+        }
+       });
+    });
+
 // end
 
 
